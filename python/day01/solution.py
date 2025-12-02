cc2c4fef53d837c3ee339b140c45881c2402ff9b
Marcin <marcinw2001@gmail.com>
2025-12-02 11:30:57 +0000

    Day 1 Part 2 partly solved (kinda bruteforced)

--- python/day01/solution.py
+++ python/day01/solution.py
@@ -30,12 +30,78 @@ def solve():
     print(f"Part I password: {pointed_at_zero}")
     
     #%   
-         
+    print()
     #% Part 2
     
     
+    current_position = 50
+    pointed_at_zero = 0
+
     
+    current_position = 50
+    pointed_at_zero = 0
+    for line in data.splitlines():
+        direction = line[0]
+        distance = int(line[1:])
+        multiple_passes = 0
+        # print(f"Dial turn [{line}] to pos ", end='')
+        
+        if direction == "R":
+            overflow = current_position + distance
+            multiple_passes = overflow // 100
+            current_position = (current_position + distance) % 100
+            
+        elif direction == "L":
+            overflow = current_position - distance
+            multiple_passes = abs(overflow // 100)
+            if current_position == 0:
+                multiple_passes -= 1 
+            current_position = (current_position - distance) % 100
+        
+        
+        # print(f"{current_position}")
+        if current_position == 0:
+            pointed_at_zero += 1
+            multiple_passes -= 1
+            # print(f"added exactly pointing at zero: [{line}]")
+        if multiple_passes > 0:
+            pointed_at_zero += multiple_passes
+            # print(f"added multiple rounds: [{line}]  {multiple_passes}")
+        
+    print(f"Part II password: {pointed_at_zero}")
+    bruteforce_part2(data)
+        
     #%
+
+def bruteforce_part2(data):
+    current_position = 50
+    pointed_at_zero = 0
+
+    for line in data.splitlines():
+        direction = line[0]
+        distance = int(line[1:])
+        
+        if direction == 'R':
+            for i in range(distance):
+                current_position += 1
+                if current_position == 100:
+                    pointed_at_zero +=1
+                    current_position = 0
+                if current_position > 100:
+                    current_position = 1
+        
+        if direction == 'L':
+            for i in range(distance):
+                current_position -= 1
+                if current_position == 0:
+                    pointed_at_zero +=1
+                    current_position = 100
+                if current_position < 0:
+                    current_position = 99
+                    
+    print("Bruteforce part 2:", pointed_at_zero)
+    return pointed_at_zero
     
 if __name__ == "__main__":
     solve()
+ 
